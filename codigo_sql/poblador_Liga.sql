5546a6bd222a89009ef26162f70b16dcdcfb7c2c
Jesus <jesusvillacampasagaste@gmail.com>
2019-04-04 19:04:50 +0200

    Liga y temporada (pobladores)

--- codigo_sql/poblador_Liga.sql
+++ codigo_sql/poblador_Liga.sql
@@ -1,3 +1,4 @@
 INSERT INTO Liga (Nom_liga) VALUES ('1ª');
 INSERT INTO Liga (Nom_liga) VALUES ('2ª');
 INSERT INTO Liga (Nom_liga) VALUES ('Promocion');
+INSERT INTO Liga (Nom_liga) VALUES ('Descenso');
